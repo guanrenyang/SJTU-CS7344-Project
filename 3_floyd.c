8cace55d0da724f8ce6fca75c67a47e3aaaf4504
guanrenyang <renyangguan@outlook.com>
2024-05-27 17:35:36 +0800

    Add parallel code for floyd.

--- 3_floyd.c
+++ 3_floyd.c
@@ -1,55 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 
 #define min(a, b) ((a) < (b) ? (a) : (b))
-
-#define N 1024
+#define N 4
 
 double A[N][N];
+double A_flat[N*N];
 
-void init_array()
-{
+void init_array() {
     int i, j;
-
     srand(123);
-    for(i= 0; i < N; i++) {
-        for(j= 0; j < N; j++) {
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
             double ra = 1.0 + ((double)N * rand() / (RAND_MAX + 1.0));
             A[i][j] = ra;
         }
     }
-    for(i= 0; i < N; i++) {
+    for (i = 0; i < N; i++) {
         A[i][i] = 0;
     }
 }
 
-void print_array()
-{
+void print_array() {
     int i, j;
-
-    for(i=0; i<N; i++) {
-        for(j=0; j<N; j++) {
-            fprintf(stderr, "%lf ", A[i][j]); 
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            fprintf(stderr, "%lf ", A[i][j]);
         }
         fprintf(stderr, "\n");
     }
 }
 
-int main()
-{
+void flat_array() {
+    int i, j;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            A_flat[i*N+j] = A[i][j];
+        }
+    }
+}
+
+void deflat_array() {
+    int i, j;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            A[i][j] = A_flat[i*N+j];
+        }
+    }
+
+}
+
+int main(int argc, char *argv[]) {
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
     init_array();
 
-#pragma scop
-    for(int k = 0; k < N; k++) {
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                A[i][j] = min(A[i][j], A[i][k] + A[k][j]);
+    flat_array();
+
+    int rows_per_process = N / size;
+    double *local_A = malloc(rows_per_process * N * sizeof(double));
+    MPI_Scatter(A_flat, rows_per_process * N, MPI_DOUBLE, local_A, rows_per_process * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    double *k_row = malloc(N * sizeof(double));
+    for (int k = 0; k < N; k++) {
+        int owner = k / rows_per_process;
+        if (rank == owner) {
+            int k_local = k % rows_per_process;
+            for (int j = 0; j < N; j++) {
+                k_row[j] = local_A[k_local * N + j];
+            }
+        }
+        MPI_Bcast(k_row, N, MPI_DOUBLE, owner, MPI_COMM_WORLD);
+
+        for (int i = 0; i < rows_per_process; i++) {
+            for (int j = 0; j < N; j++) {
+                local_A[i * N + j] = min(local_A[i * N + j], local_A[i * N + k] + k_row[j]);
             }
         }
     }
-#pragma endscop
 
-    print_array();
+    MPI_Gather(local_A, rows_per_process * N, MPI_DOUBLE, A_flat, rows_per_process * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+    deflat_array();
+
+    if (rank == 0) {
+        print_array();
+    }
+
+    free(local_A);
+    free(k_row);
+    MPI_Finalize();
     return 0;
 }
--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# Makefile to compile and run MPI and serial ADI programs
+# Makefile to compile and run MPI and serial ADI programs as well as Floyd's algorithm programs
 
 # Compiler settings
 MPICC=mpicc
@@ -8,24 +8,42 @@ CFLAGS=-Wall -O2
 MPICFLAGS=$(CFLAGS)
 LDFLAGS=
 
-# MPI program
+# MPI program for ADI
 MPI_SRC=1_adi.c
 MPI_EXEC=adi
 
-# Serial program
+# Serial program for ADI
 SERIAL_SRC=1_adi_serial.c
 SERIAL_EXEC=adi_serial
 
-# Build and run the MPI program
+# MPI program for Floyd's algorithm
+FLOYD_MPI_SRC=3_floyd.c
+FLOYD_MPI_EXEC=floyd
+
+# Serial program for Floyd's algorithm
+FLOYD_SERIAL_SRC=3_floyd_serial.c
+FLOYD_SERIAL_EXEC=floyd_serial
+
+# Build and run the MPI program for ADI
 adi: $(MPI_SRC)
 	$(MPICC) $(MPICFLAGS) -o $(MPI_EXEC) $(MPI_SRC) $(LDFLAGS)
 	$(MPIRUN) -np 4 ./$(MPI_EXEC)
 
-# Build and run the serial program
+# Build and run the serial program for ADI
 adi_serial: $(SERIAL_SRC)
 	$(CC) $(CFLAGS) -o $(SERIAL_EXEC) $(SERIAL_SRC) $(LDFLAGS)
 	./$(SERIAL_EXEC)
 
+# Build and run the MPI program for Floyd's algorithm
+floyd: $(FLOYD_MPI_SRC)
+	$(MPICC) $(MPICFLAGS) -o $(FLOYD_MPI_EXEC) $(FLOYD_MPI_SRC) $(LDFLAGS)
+	$(MPIRUN) -np 4 ./$(FLOYD_MPI_EXEC)
+
+# Build and run the serial program for Floyd's algorithm
+floyd_serial: $(FLOYD_SERIAL_SRC)
+	$(CC) $(CFLAGS) -o $(FLOYD_SERIAL_EXEC) $(FLOYD_SERIAL_SRC) $(LDFLAGS)
+	./$(FLOYD_SERIAL_EXEC)
+
 # Clean up build files
 clean:
-	rm -f $(MPI_EXEC) $(SERIAL_EXEC)
+	rm -f $(MPI_EXEC) $(SERIAL_EXEC) $(FLOYD_MPI_EXEC) $(FLOYD_SERIAL_EXEC)
